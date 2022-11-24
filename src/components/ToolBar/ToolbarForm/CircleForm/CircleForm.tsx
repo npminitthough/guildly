2d3ed7812908fbb03ba1435bc802a786ecc2a7d4
Tasnim Sultanah <tasnim.sultanah@trulyexperiences.com>
2022-11-24 10:43:39 +0000

    Move Toolbar forms to a Toolbar component folder and connect them to context.

---
+++ src/components/ToolBar/ToolbarForm/CircleForm/CircleForm.tsx
@@ -0,0 +1,62 @@
+import { useState, useContext } from "react";
+
+import ToolBarForm from "../ToolBarForm";
+import Input from "../../../common/Input/Input";
+import Button from "../../../common/Button/Button";
+
+import { DesignContext } from "../../../../store/design-context";
+import { CardType } from "../../../../ts/enums";
+
+export default function CircleForm() {
+  const designCtx = useContext(DesignContext);
+
+  const [formState, setFormState] = useState({
+    diameter: undefined,
+    name: undefined,
+  });
+
+  function onInputChange (e:any) {
+    setFormState(curr => {
+      return {...curr, [e.target.name]: e.target.value}
+    })
+  }
+
+  function onSubmit(e: any) {
+    e.preventDefault();
+    
+    const { diameter, name } = formState;
+    if (diameter) {
+      designCtx.addToCanvas({
+        type: CardType.custom,
+        dimensions: {
+          xInMetres: diameter,
+        },
+        name,
+      });
+    }
+  }
+
+  return (
+    <ToolBarForm onSubmit={onSubmit}>
+      <Input
+        label="label"
+        name="name"
+        value={formState.name || ''}
+        type="text"
+        id="custom-circle-label"
+        onChange={onInputChange}
+      />
+      <Input
+        label="diameter (m)"
+        required
+        name="diameter"
+        value={formState.diameter || ''}
+        type="number"
+        width="50px"
+        id="custom-circle-diameter"
+        onChange={onInputChange}
+      />
+      <Button type="submit">Add</Button>
+    </ToolBarForm>
+  );
+}
---
+++ src/components/ToolBar/ToolbarForm/RectForm/RectForm.stories.js
@@ -0,0 +1,10 @@
+import RectForm from './RectForm';
+
+export default {
+  title: "RectForm",
+  component: RectForm,
+};
+
+export const MyRectForm = () => {
+  return <RectForm />
+}
