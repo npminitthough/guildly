dab268c4314597939549027cb1e25a05b5948f1a
Tasnim Sultanah <tasnim.sultanah@trulyexperiences.com>
2022-11-26 18:44:14 +0000

    Update logo in Nav component to function as a Link.

--- src/components/common/Nav/Nav.tsx
+++ src/components/common/Nav/Nav.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 import GlobalStyles from "../../../constants/styles";
@@ -10,32 +11,34 @@ interface IProps {
 
 function Nav({ children, height }: IProps) {
   return (
-    <StyledNav id='navbar' height={height}>
-      <Logo>
-        <p>Guildly</p>
-      </Logo>
+    <StyledNav id="navbar" height={height}>
+      <Logo to="/">Guildly</Logo>
       <Contents>{children}</Contents>
     </StyledNav>
   );
 }
 
-const StyledNav = styled.nav<{height?: string}>`
+const StyledNav = styled.nav<{ height?: string }>`
   width: 100vw;
   ${({ height }) => {
     return `
-      height: ${height || '40px'};
-    `
+      height: ${height || "40px"};
+    `;
   }}
   background-color: ${GlobalStyles.colors.primary600};
   display: flex;
   color: white;
 `;
 
-const Logo = styled.div`
+const Logo = styled(Link)`
   flex: 1;
   display: flex;
   justify-content: space-around;
   align-items: center;
+  color: white;
+  text-decoration: none;
+  font-size: 1.3em;
+  font-family: "Kirang Haerang", "Arial";
 `;
 
 const Contents = styled.div`
