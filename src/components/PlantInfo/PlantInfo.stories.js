78be06bdc2fcb82ff92c1a09d944e695fb2bda62
Tasnim Sultanah <tasnim.sultanah@trulyexperiences.com>
2023-01-26 22:59:57 +0000

    Replace tags with icons in <PlantInfo> and make it optionally clickable. Add plant name to <PlantInfo>.

--- src/components/PlantInfo/PlantInfo.stories.js
+++ src/components/PlantInfo/PlantInfo.stories.js
@@ -10,11 +10,42 @@ const data =  {
   widthInMetres: 4,
   heightInMetres: 6,
   nitrogenFixer: true,
-  insectAttractor: true
+  imageUrl: "https://www.coopersofstortford.co.uk/images/products/medium/XYG81i.jpg"
+}
+
+const dataWithName =  {
+  name: "siberian pea tree",
+  widthInMetres: 4,
+  heightInMetres: 6,
+  nitrogenFixer: true,
+  insectAttractor: true,
+  nutrientAccumulator: true,
+  imageUrl: "https://www.coopersofstortford.co.uk/images/products/medium/XYG81i.jpg"
+}
+
+const dataWithNameOneTag =  {
+  name: "siberian pea tree",
+  widthInMetres: 4,
+  heightInMetres: 6,
+  nitrogenFixer: true,
+  rootStock: 'quince C',
+  imageUrl: "https://www.coopersofstortford.co.uk/images/products/medium/XYG81i.jpg"
 }
 
 export const MyPlantInfo = () => {
   return (
       <PlantInfo id="thing" plant={data} />
+  );
+};
+
+export const MyPlantInfoWithName = () => {
+  return (
+      <PlantInfo id="thing" displayName={true} plant={dataWithName} />
+  );
+};
+
+export const MyPlantInfoWithNameOneTag = () => {
+  return (
+      <PlantInfo id="thing" displayName={true} plant={dataWithNameOneTag} />
   );
 };
